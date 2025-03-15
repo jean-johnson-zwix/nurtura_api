9aa7e5bdde9d1f5400af1e59be822587443846bd
Jean Johnson <jajoh151@asu.edu>
2025-03-14 18:43:08 -0700

    feat: create user profile api

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import model
+from config import engine
+from router import router
+import config
+from functools import lru_cache
+
+model.Base.metadata.create_all(bind=engine)
+app = FastAPI()
+
+@lru_cache
+def get_settings():
+    return config.Settings()
+
+@app.get('/')
+async def hello_world():
+    return {"message":"Hellow World"}
+
+app.include_router(router,prefix="/auth", tags=["user management"])
---
+++ app/schemas.py
@@ -0,0 +1,25 @@
+from typing import Optional, Generic, TypeVar
+from pydantic import BaseModel, Field
+
+T = TypeVar('T')
+
+class CreateUserProfileRequest(BaseModel):
+    user_name: str
+    password: str
+    first_name: str
+    last_name: str
+    email: str
+    age: int
+    height: float
+    weight: float
+
+class CreateUserProfileResponse(BaseModel):
+    user_id: int
+    user_name: str
+
+class Response(BaseModel, Generic[T]):
+    status_code: int
+    message: str
+    result: Optional[T] = None
+
+
