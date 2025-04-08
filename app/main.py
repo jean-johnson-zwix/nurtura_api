cc98dbc5c5c346818cf8fa7116c8032da7d8e6b8
Jean Johnson <jajoh151@asu.edu>
2025-04-08 01:44:52 -0700

    feat: enable cors for dev

--- app/main.py
+++ app/main.py
@@ -4,9 +4,35 @@ from config import engine
 from router import router
 import config
 from functools import lru_cache
+from fastapi.middleware.cors import CORSMiddleware
+
 
 model.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+
+'''
+origins = [
+    "http://localhost:3000",
+    "https://your-frontend-domain.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
+'''
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @lru_cache
 def get_settings():
